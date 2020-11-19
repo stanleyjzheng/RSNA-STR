fb505b756fc4dc4d0fa4014c284e20015e0e6f47
stanley-zheng <stanleyjzheng@gmail.com>
2020-11-18 17:41:03 -0700

    change paths

--- dataset.sh
+++ dataset.sh
@@ -1,29 +1,7 @@
-module load anaconda3/3.7
+wget -O rsna-str-pulmonary-embolism-detection.zip "https://storage.googleapis.com/kaggle-competitions-data/kaggle-v2/22307/1502524/bundle/archive.zip?GoogleAccessId=web-data@kaggle-161607.iam.gserviceaccount.com&Expires=1605995752&Signature=lRsb3PAOxs8R3epPP4PaX6gQ3Xb%2FP2lSZp7PWSjGisIlyHrFVnDabRJMo6phLafGmMFBY9tqFOSUQwGvFqRu9xldphuwq5lGgvXYUAG0aTq3WjUedyuEzPM4FSEk7UvQJAZ6LceDRbKjfsfSi1hNbFV%2FLKkHfyr22nOrNX4sCRbDWirwMNv8MMyInYl%2FaaD0%2Bb8%2BZCPBX9IifeuM6U3fq9K86ey1zEUVLLNHVXOVQfq9Ow5q%2BIiuYRcu7ydoTv6FDJq0Cu%2BN%2FvlJWZ3L9vKeynCpAeWYxbOhDPJfFPheHOLZ3j%2FVeEMI7mLgiISW4Htt%2FhtpLtzOwcYiVE%2FuhyfoxQ%3D%3D&response-content-disposition=attachment%3B+filename%3Drsna-str-pulmonary-embolism-detection.zip"
 
-echo Enter your Kaggle username:
-read username 
-echo Enter your Kaggle API key:
-read api_key 
-
-mkdir $HOME/.kaggle
-echo '{"username":'$username', "key":'$api_key'}' > $HOME/.kaggle/kaggle.json
-
-echo Installing Kaggle API
-pip install kaggle -q
-
-cd input
-
-echo Downloading datasets 
-kaggle competitions download -c rsna-str-pulmonary-embolism-detection
 mkdir rsna-str-pulmonary-embolism-detection
+echo unzipping dataset 
 
-kaggle datasets download -d khyeh0719/kh-rsna-model
-mkdir kh-rsna-model 
-
-echo Downloaded datasets, unzipping datasets
-
-unzip -q rsna-str-pulmonary-embolism-detection.zip rsna-str-pulmonary-embolism-detection
-unzip -q kh-rsna-model.zip kh-rsna-model
-
-rm -f *.zip
-chmod 600 $HOME/.kaggle
+unzip -q rsna-str-pulmonary-embolism-detection.zip -d rsna-str-pulmonary-embolism-detection
+rm -f rsna-str-pulmonary-embolism-detection.zip
